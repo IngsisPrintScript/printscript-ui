7f48a8eacafc201ec1d4ac999046d73da0664166
Santino2005 <santinocolombo13@gmail.com>
2025-12-07 15:33:01 -0300

    Test 1

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,13 @@ import './index.css'
 import {createRoot} from "react-dom/client";
 import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
-import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ""}
-            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
-            authorizationParams={{
-                redirect_uri: window.location.origin
-            }}
-        >
-            <PaginationProvider>
-                <SnackbarProvider>
-                    <App/>
-                </SnackbarProvider>
-            </PaginationProvider>
-        </Auth0Provider>
+        <PaginationProvider>
+            <SnackbarProvider>
+                <App/>
+            </SnackbarProvider>
+        </PaginationProvider>
     </React.StrictMode>,
 )
